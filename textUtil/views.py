29e9c45b1c02ee05608312d5b0fbab21a6db1d36
xort <a.abhigyanam@gmail.com>
2023-11-16 13:57:50 +0530

    new features added

--- textUtil/views.py
+++ textUtil/views.py
@@ -1,6 +1,4 @@
 #Created by xort
-
-
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -8,21 +6,44 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 def analyze(request):
-    #get the text
+    # Get the text
     djText = request.GET.get('text', 'default')
     
-    print(djText)
+    # Check box value
     removepunc = request.GET.get('removepunc', 'off')
-    print(removepunc)
-    #remove punctuation
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newLineRemover = request.GET.get('newLineRemover', 'off')
+    extraSpaceRemover = request.GET.get('extraSpaceRemover', 'off')    
     
     punctuations = '''!"#$%&'()*+,- ./:;<=>?@[\]^_`{|}~'''
-    analyzed = ""
     if(removepunc == "on"):
+        analyzed = ""
         for char in djText:
             if(char not in punctuations):
                 analyzed += char
+        params = {'purpose':'Remove Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif(fullcaps == "on"):
+        analyzed = ""
+        for char in djText:
+            analyzed += char.upper()
+        params = {'purpose' : 'Upper case', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif(newLineRemover == "on"):
+        analyzed = ""
+        for char in djText:
+            if(char != "\n"):
+                analyzed += char
+        params = {'purpose' : 'New line removed', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif(extraSpaceRemover == "on"):
+        analyzed = ""
+        for index, char in enumerate(djText):
+            if(not(djText[index] == " " and djText[index + 1] == " ")):
+                analyzed += char
+        params = {'purpose' : 'Extra Space removed', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
     else:
         return HttpResponse("errore")
-    params = {'purpose':'Remove Punctuations', 'analyzed_text': analyzed}
-    return render(request, 'analyze.html', params)
+    
+    
